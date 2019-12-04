fdb8f3d2db8a125d09368cf7dfbee3959c4f5426
ApamAa <apama.jamshidi@gmail.com>
2019-12-04 22:55:23 +0000

    client.js

--- client.js
+++ client.js
@@ -11,6 +11,10 @@ const connect = function(data) {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+  });
+  conn.write('Name: AP');
   conn.on('data', (data) => {
     console.log(data);
   });
