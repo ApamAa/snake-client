6cc9d4148d36a75d4aa0d2f4806459e4d98b6c99
ApamAa <apama.jamshidi@gmail.com>
2019-12-05 02:17:57 +0000

    input.js

--- input.js
+++ input.js
@@ -1,19 +1,40 @@
+let connection; 
+
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log(`Thanks for using me, ciao!`);
     process.exit();
+  } 
+  if (key === 'w') {
+    //console.log("Up");
+    connection.write('Move: up')
+  } else if ( key === 'a') {
+    //console.log("left")
+    connection.write('Move: left')
+  } else if (key === 's') {
+    //console.log("down")
+    connection.write('Move: down')
+  } else if (key === 'd') {
+    //console.log("right")
+    connection.write('Move: right')
   }
+
+
 }
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.on('data', handleUserInput)
+
   stdin.resume();
   
   return stdin;
     
 }
 
+
 module.exports = { setupInput };
+
