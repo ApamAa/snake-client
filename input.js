bb15a0f180492a766739cd0dc5138da102ef8da6
ApamAa <apama.jamshidi@gmail.com>
2019-12-05 00:50:56 +0000

    input.js

---
+++ input.js
@@ -0,0 +1,19 @@
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log(`Thanks for using me, ciao!`);
+    process.exit();
+  }
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput)
+  stdin.resume();
+  
+  return stdin;
+    
+}
+
+module.exports = { setupInput };
